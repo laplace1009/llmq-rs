a1bf4f46008d3e0f9317abee6870290d0fc589de
부익 김 <laplace1009@gmail.com>
2024-06-21 23:04:18 +0900

    Message, Consumer, Exchange, Channel 구조체 기본 정의

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::collections::{HashMap, HashSet, VecDeque};
 use tokio::net::TcpListener;
 use std::io;
+use std::ops::DerefMut;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -10,22 +12,60 @@ pub enum ServerError {
     PortInUse(u16)
 }
 
-struct Producer {
-    name: String,
+struct Message {
+    message: Vec<u8>
 }
 
 struct Consumer {
     name: String,
 }
 
+struct Exchange {
+    message_queues: HashMap<String, VecDeque<Vec<u8>>>
+}
+
+impl Exchange {
+    fn new() -> Self {
+        Exchange {
+            message_queues: HashMap::new()
+        }
+    }
+}
+
 struct Channel {
-    producers: Vec<Producer>,
-    consumer: Vec<Consumer>,
+    consumers: Vec<Consumer>,
+    exchanges: HashMap<String, Exchange>,
+}
+impl Channel {
+    fn new() -> Self {
+        Channel {
+            consumers: Vec::new(),
+            exchanges: HashMap::new(),
+        }
+    }
+
+    #[allow(unused)]
+    async fn basic_publish(&mut self, exchange: String, routing_key: String, message: String) -> Result<(), io::Error> {
+        if let Some(ex) = self.find_exchange(&exchange) {
+
+        }
+        todo!()
+    }
+
+    fn find_exchange(&mut self, exchange: &str) -> Option<&mut Exchange> {
+        self.exchanges.get_mut(exchange)
+    }
+
+    fn find_queue<'a>(exchange: &'a mut Exchange, routing_key: &str) -> Option<&'a mut VecDeque<Vec<u8>>> {
+        exchange.message_queues.get_mut(routing_key)
+    }
 }
 
 struct Server {
     tcp_listener: TcpListener,
+    channels: Vec<Channel>,
 }
+
 
 impl Server {
     async fn new(address: &'static str) -> Result<Self, ServerError> {
@@ -41,6 +81,7 @@ impl Server {
 
         Ok(Server {
             tcp_listener,
+            channels: Vec::new(),
         })
     }
 }
